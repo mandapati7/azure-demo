53bb5b920749d93737a0ca1afb80577bafb77144
Gopikrishna Mandapati <mandapati.g.c@gmail.com>
2021-12-20 00:31:13 -0500

    init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+
+@RestController
+@RequestMapping("/")
+class RestService {
+	
+	
+	@GetMapping("/greet")
+	@ResponseBody
+	public String getGreetings() {
+		return "Hello "+new Date();
+	}
+	
+}
